a2ef7a78150746e6902087d140eb2fea422a11d1
gordtulloch <gord.tulloch@gmail.com>
2020-08-27 15:51:01 -0500

    Update pihqccd.cpp

--- pihqccd.cpp
+++ pihqccd.cpp
@@ -4,7 +4,7 @@
    **   Version 0.0                                                                                        **
    **********************************************************************************************************/
 
-#include "pihqccd.h"
+#include "pihqccd.h" 
 #include <memory>
 
 /* Macro shortcut to CCD temperature value */
